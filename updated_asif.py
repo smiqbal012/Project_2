782bcf7918796dededa1cbbde64f86d56ae9f15f
smiqbal012 <144156940+smiqbal012@users.noreply.github.com>
2023-11-14 05:23:59 -0500

    black implemented for formatting

--- updated_asif.py
+++ updated_asif.py
@@ -4,11 +4,14 @@ import time
 from scipy.integrate import odeint
 import cProfile
 
+
 def pdf(x, k):
     return k * np.exp(-x)
 
+
 def cdf(x, k):
     return 1 - np.exp(-k * x)
+
 
 def rejection_sampling(pdf, cdf, k, num_samples):
     samples = []
@@ -18,6 +21,7 @@ def rejection_sampling(pdf, cdf, k, num_samples):
         if u < pdf(x, k) / (k * cdf(x, k)):
             samples.append(x)
     return samples
+
 
 class Particle:
     def __init__(self):
@@ -31,12 +35,13 @@ class Particle:
     def acceleration(self, E):
         return self.charge * E / self.mass
 
+
 class Simulation:
     def __init__(self, num_particles=1000, E=0.5, dt=0.1, T=5, k=3.0):
         self.particles = [Particle() for _ in range(num_particles)]
         self.E = E
         self.dt = dt
-        self.time_steps = int(T/dt)
+        self.time_steps = int(T / dt)
         self.positions_odeint = []
         self.positions_rk4 = []
         self.k = k
@@ -53,18 +58,28 @@ class Simulation:
         k2 = dt * np.array(func(y + 0.5 * k1, t + 0.5 * dt, *args))
         k3 = dt * np.array(func(y + 0.5 * k2, t + 0.5 * dt, *args))
         k4 = dt * np.array(func(y + k3, t + dt, *args))
-        return y + (k1 + 2*k2 + 2*k3 + k4) / 6
+        return y + (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
     def evolve(self):
         t = np.linspace(0, 5, self.time_steps)
         for p in self.particles:
             y0 = [p.position, p.velocity]
-            sol_odeint = odeint(self.particle_ode, y0, t, args=(p.charge, p.mass, self.E))[:,0]
+            sol_odeint = odeint(
+                self.particle_ode, y0, t, args=(p.charge, p.mass, self.E)
+            )[:, 0]
             self.positions_odeint.append(sol_odeint)
 
             sol_rk4 = [y0]
             for i in range(1, len(t)):
-                next_step = self.rk4(sol_rk4[-1], t[i-1], self.dt, self.particle_ode, p.charge, p.mass, self.E)
+                next_step = self.rk4(
+                    sol_rk4[-1],
+                    t[i - 1],
+                    self.dt,
+                    self.particle_ode,
+                    p.charge,
+                    p.mass,
+                    self.E,
+                )
                 sol_rk4.append(next_step)
             self.positions_rk4.append([pos_vel[0] for pos_vel in sol_rk4])
 
@@ -79,26 +94,42 @@ class Simulation:
         total_kinetic_energy = np.sum(self.kinetic_energy, axis=0)
         plt.figure(figsize=(8, 6))
         plt.plot(time, total_kinetic_energy)
-        plt.title('Total Kinetic Energy vs. Time')
-        plt.xlabel('Time (s)')
-        plt.ylabel('Total Kinetic Energy')
+        plt.title("Total Kinetic Energy vs. Time")
+        plt.xlabel("Time (s)")
+        plt.ylabel("Total Kinetic Energy")
         plt.grid(True)
         plt.savefig("KineticEnergyVsTime_k3_1000.png")
         plt.show()
 
     def plot_positions(self):
-        times_to_plot = [0, int(2.5/self.dt), int(5/self.dt) - 1]
+        times_to_plot = [0, int(2.5 / self.dt), int(5 / self.dt) - 1]
         plt.figure(figsize=(15, 5))
         for i, time_idx in enumerate(times_to_plot):
-            plt.subplot(1, 3, i+1)
+            plt.subplot(1, 3, i + 1)
             positions_at_time = [sol[time_idx] for sol in self.positions_odeint]
             charges = [p.charge for p in self.particles]
-            pos_positions = [pos for pos, charge in zip(positions_at_time, charges) if charge >= 0]
-            neg_positions = [pos for pos, charge in zip(positions_at_time, charges) if charge < 0]
-            plt.scatter(range(len(pos_positions)), pos_positions, s=1, color='b', label='Positive Charge')
-            plt.scatter(range(len(neg_positions)), neg_positions, s=1, color='r', label='Negative Charge')
+            pos_positions = [
+                pos for pos, charge in zip(positions_at_time, charges) if charge >= 0
+            ]
+            neg_positions = [
+                pos for pos, charge in zip(positions_at_time, charges) if charge < 0
+            ]
+            plt.scatter(
+                range(len(pos_positions)),
+                pos_positions,
+                s=1,
+                color="b",
+                label="Positive Charge",
+            )
+            plt.scatter(
+                range(len(neg_positions)),
+                neg_positions,
+                s=1,
+                color="r",
+                label="Negative Charge",
+            )
             plt.title(f"Particle Positions at t={time_idx*self.dt}s")
-            plt.ylim(-15,15)
+            plt.ylim(-15, 15)
             plt.xlabel("Particle Index")
             plt.ylabel("Position")
             plt.legend()
@@ -118,7 +149,9 @@ class Simulation:
         return initial_masses, initial_charges
 
     def perform_interactions(self):
-        interaction_positions = rejection_sampling(pdf, cdf, self.k, len(self.particles))
+        interaction_positions = rejection_sampling(
+            pdf, cdf, self.k, len(self.particles)
+        )
         interaction_threshold = 0.5
         for particle, position in zip(self.particles, interaction_positions):
             if position < interaction_threshold:
@@ -128,22 +161,33 @@ class Simulation:
     def plot_comparison(self):
         time_idx = np.abs(np.linspace(0, 5, self.time_steps) - 4.9).argmin()
         plt.figure(figsize=(12, 6))
-        for i, (particle, pos_odeint, pos_rk4) in enumerate(zip(self.particles, self.positions_odeint, self.positions_rk4)):
+        for i, (particle, pos_odeint, pos_rk4) in enumerate(
+            zip(self.particles, self.positions_odeint, self.positions_rk4)
+        ):
             if particle.charge > 0:
-                plt.scatter(i, pos_odeint[time_idx], color='b', s=10, label='odeint' if i == 0 else "")
-                plt.scatter(i, pos_rk4[time_idx], color='r', s=10, label='RK4' if i == 0 else "")
-        plt.title('Comparison of Positively Charged Particle Positions at t=4.9s')
-        plt.xlabel('Particle Index')
-        plt.ylabel('Position')
+                plt.scatter(
+                    i,
+                    pos_odeint[time_idx],
+                    color="b",
+                    s=10,
+                    label="odeint" if i == 0 else "",
+                )
+                plt.scatter(
+                    i, pos_rk4[time_idx], color="r", s=10, label="RK4" if i == 0 else ""
+                )
+        plt.title("Comparison of Positively Charged Particle Positions at t=4.9s")
+        plt.xlabel("Particle Index")
+        plt.ylabel("Position")
         plt.legend()
         plt.ylim(0, 80)
         plt.grid(True)
         plt.savefig("PositionComparison_odeint_RK4_at_4.9s.png")
         plt.show()
 
+
 if __name__ == "__main__":
     sim = Simulation(k=3.0)
-    cProfile.runctx('sim.evolve()', globals(), locals(), 'evolve_stats.prof')
+    cProfile.runctx("sim.evolve()", globals(), locals(), "evolve_stats.prof")
     sim.evolve()
     sim.calculate_kinetic_energy()
     sim.plot_kinetic_energy()
@@ -154,18 +198,18 @@ if __name__ == "__main__":
     initial_masses, initial_charges = sim.get_initial_distributions()
     plt.figure(figsize=(12, 5))
     plt.subplot(1, 2, 1)
-    plt.hist(initial_masses, bins=50, color='b', alpha=0.5, label='Initial')
-    plt.hist(final_masses, bins=50, color='r', alpha=0.5, label='Final')
-    plt.title('Mass Distribution')
-    plt.xlabel('Mass')
-    plt.ylabel('Count')
+    plt.hist(initial_masses, bins=50, color="b", alpha=0.5, label="Initial")
+    plt.hist(final_masses, bins=50, color="r", alpha=0.5, label="Final")
+    plt.title("Mass Distribution")
+    plt.xlabel("Mass")
+    plt.ylabel("Count")
     plt.legend()
     plt.subplot(1, 2, 2)
-    plt.hist(initial_charges, bins=50, color='g', alpha=0.5, label='Initial')
-    plt.hist(final_charges, bins=50, color='m', alpha=0.5, label='Final')
-    plt.title('Charge Distribution')
-    plt.xlabel('Charge')
-    plt.ylabel('Count')
+    plt.hist(initial_charges, bins=50, color="g", alpha=0.5, label="Initial")
+    plt.hist(final_charges, bins=50, color="m", alpha=0.5, label="Final")
+    plt.title("Charge Distribution")
+    plt.xlabel("Charge")
+    plt.ylabel("Count")
     plt.legend()
     plt.tight_layout()
     plt.savefig("MassChargeDistributions_k3_1000.png")
@@ -180,10 +224,10 @@ if __name__ == "__main__":
         runtime = end_time - start_time
         runtimes.append(runtime)
     plt.figure(figsize=(8, 6))
-    plt.plot(particle_counts, runtimes, marker='o')
-    plt.title('Simulation Evolution Time vs. Number of Particles')
-    plt.xlabel('Number of Particles')
-    plt.ylabel('Time (seconds)')
+    plt.plot(particle_counts, runtimes, marker="o")
+    plt.title("Simulation Evolution Time vs. Number of Particles")
+    plt.xlabel("Number of Particles")
+    plt.ylabel("Time (seconds)")
     plt.grid(True)
     plt.savefig("SimulationTimeVsParticles.png")
     plt.show()
